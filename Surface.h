09d162c871f14201d6800f0856bd9ef32b6e784b
Willem van der Schyff <willem@spiraleye.com>
2012-07-03 21:51:44 +0200

    Add Transparency

--- Surface.h
+++ Surface.h
@@ -9,6 +9,7 @@ public:
   static SDL_Surface* Load(char* filename);
   static bool Draw(SDL_Surface* destSurface, SDL_Surface* srcSurface, int x, int y);
   static bool DrawRegion(SDL_Surface* destSurface, SDL_Surface* srcSurface, int x, int y, int x2, int y2, int w, int h);
+  static bool Transparent(SDL_Surface* destSurface, int R, int G, int B);
 };
 
 #endif
