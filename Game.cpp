fc6f00785d8fec9b7ada75824e3a5a1d35cb5ffa
Willem van der Schyff <willem@spiraleye.com>
2012-07-03 21:04:08 +0200

    XOXO Gossip Girl

--- Game.cpp
+++ Game.cpp
@@ -3,7 +3,9 @@
 Game::Game() {
   running = true;
   displaySurface = NULL;
-  testSurface = NULL;
+  gridSurface = NULL;
+  xSurface = NULL;
+  oSurface = NULL;
 }
 
 int Game::OnExecute() {
@@ -32,21 +34,31 @@ bool Game::OnInit() {
     return false;
   }
 
-  if ((displaySurface = SDL_SetVideoMode(800, 600, 32, SDL_HWSURFACE | SDL_DOUBLEBUF)) == NULL) {
+  if ((displaySurface = SDL_SetVideoMode(600, 600, 32, SDL_HWSURFACE | SDL_DOUBLEBUF)) == NULL) {
     return false;
   }
 
-  if ((testSurface = Surface::Load("C:\\Users\\Willem\\Documents\\Visual Studio 11\\Projects\\Cheddamelt\\Debug\\fry.bmp")) == NULL) {
+  if ((gridSurface = Surface::Load("C:\\Users\\Willem\\Documents\\Visual Studio 11\\Projects\\Cheddamelt\\Debug\\grid.bmp")) == NULL) {
     return false;
   }
+
+  if ((xSurface = Surface::Load("C:\\Users\\Willem\\Documents\\Visual Studio 11\\Projects\\Cheddamelt\\Debug\\x.bmp")) == NULL) {
+    return false;
+  }
+
+  if ((oSurface = Surface::Load("C:\\Users\\Willem\\Documents\\Visual Studio 11\\Projects\\Cheddamelt\\Debug\\o.bmp")) == NULL) {
+    return false;
+  }
+
+  Surface::Transparent(xSurface, 255, 0, 255);
+  Surface::Transparent(oSurface, 255, 0, 255);
+
+  Reset();
 
   return true;
 }
 
 void Game::OnEvent(SDL_Event* event) {
-  /*if (event->type == SDL_QUIT) {
-    running = false;
-  }*/
   Event::OnEvent(event);
 }
 
@@ -54,17 +66,63 @@ void Game::OnExit() {
   running = false;
 }
 
+void Game::OnLButtonDown(int x, int y) {
+  int id = x / 200;
+  id = id + ((y / 200) * 3);
+
+  if (grid[id] != GRID_TYPE_NONE) {
+    return;
+  }
+
+  if (currentPlayer == 0) {
+    SetCell(id, GRID_TYPE_X);
+    currentPlayer = 1;
+  }
+  else {
+    SetCell(id, GRID_TYPE_O);
+    currentPlayer = 0;
+  }
+}
+
 void Game::OnLoop() {
 }
 
 void Game::OnRender() {
-  Surface::Draw(displaySurface, testSurface, 0, 0);
-  Surface::DrawRegion(displaySurface, testSurface, 400, 0, 0, 0, 198, 140);
+  Surface::Draw(displaySurface, gridSurface, 0, 0);
+
+  for (int i = 0; i < 9; i++) {
+    int x = (i % 3) * 200;
+    int y = (i / 3) * 200;
+
+    if (grid[i] == GRID_TYPE_X) {
+      Surface::Draw(displaySurface, xSurface, x, y);
+    }
+    else if (grid[i] == GRID_TYPE_O) {
+      Surface::Draw(displaySurface, oSurface, x, y);
+    }
+  }
+
   SDL_Flip(displaySurface);
 }
 
 void Game::OnCleanup() {
-  SDL_FreeSurface(testSurface);
+  SDL_FreeSurface(oSurface);
+  SDL_FreeSurface(xSurface);
+  SDL_FreeSurface(gridSurface);
   SDL_FreeSurface(displaySurface);
   SDL_Quit();
+}
+
+void Game::Reset() {
+  currentPlayer = 0;
+  for (int i = 0; i < 9; i++) {
+    grid[i] = GRID_TYPE_NONE;
+  }
+}
+
+void Game::SetCell(int id, int type) {
+  if (id < 0 || id >= 9) return;
+  if (type < GRID_TYPE_NONE || type > GRID_TYPE_O) return;
+
+  grid[id] = type;
 }
--- Game.h
+++ Game.h
@@ -10,16 +10,35 @@ class Game : public Event {
 private:
   bool running;
   SDL_Surface* displaySurface;
-  SDL_Surface* testSurface;
+  
+  SDL_Surface* gridSurface;
+  SDL_Surface* xSurface;
+  SDL_Surface* oSurface;
+
+  int grid[9];
+  int currentPlayer;
+
+  enum {
+    GRID_TYPE_NONE = 0,
+    GRID_TYPE_X,
+    GRID_TYPE_O
+  };
+
 public:
   Game();
   int OnExecute();
   bool OnInit();
+  
   void OnEvent(SDL_Event* event);
+  void OnLButtonDown(int x, int y);
+  
   void OnExit();
   void OnLoop();
   void OnRender();
   void OnCleanup();
+  void Reset();
+
+  void SetCell(int id, int type);
 };
 
 #endif
